78a8bacb33af8345e0327dde879540606eccb289
Nathan Pelletier <nathantylerpelletier@gmail.com>
2023-08-30 14:11:41 -0600

    Style choice, kept end bracket with parent

--- 2023/JetpackComposeTutorial/app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ 2023/JetpackComposeTutorial/app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                    color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
                 }//Surface
             }//JetpackComposeTutorialTheme
