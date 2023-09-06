bed594222e687102c591c27ce898fb8ddae0711e
Nathan Pelletier <nathantylerpelletier@gmail.com>
2023-09-05 19:08:43 -0600

    Quickly updated project to chapter 4.
    If i didn't lose my progress, this is where I would've been.

--- 2023/JetpackComposeTutorial/app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ 2023/JetpackComposeTutorial/app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +26,7 @@ class MainActivity : ComponentActivity() {
       JetpackComposeTutorialTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+
         }//Surface
       }//JetpackComposeTutorialTheme
     }//setContent
@@ -27,20 +34,22 @@ class MainActivity : ComponentActivity() {
 }//ComponentActivity
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(text = "Hello $name!", modifier = modifier)
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+  Column(verticalArrangement = Arrangement.Center, modifier = modifier.padding(8.dp)) {
+    Text(text = message, fontSize = 100.sp, lineHeight = 116.sp, textAlign = TextAlign.Center)
+    Text(text = from, fontSize = 36.sp,
+      modifier = Modifier
+        .padding(16.dp)
+        .align(alignment = Alignment.End))
+  }//Column
 }//Greeting
 
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-  JetpackComposeTutorialTheme {
-    Greeting("Android")
-  }//JetpackComposeTutorialTheme
-}//GreetingPreview
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun BirthdayCardPreview() {
+  JetpackComposeTutorialTheme {
+    GreetingText(message = "Happy Birthday Mom!", from = "Love Nathan")
+  }//JetPackComposeTutorialTheme
 
 }//BirthdayCardPreview
